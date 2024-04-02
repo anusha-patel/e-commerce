9a05d035debf40b6a1f661e7a73808daf69e211e
anusha-patel <chandraleka1696@gmail.com>
2024-04-02 16:32:05 +0530

    cors update

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ import authRoutes from './routes/authRoute.js';
 import cors from 'cors';
 import categoryRoutes from './routes/categoryRoutes.js'
 import productRoutes from './routes/productRoute.js'
+// import path from 'path';
 
 //configure env with dotenv.config({path:''})
 // because our .env file is in root we did not use path
@@ -21,24 +22,42 @@ const app = express();
 
 //middleware
 // app.use(cors());
+// app.options('*', cors({
+//     origin: ["http://localhost:3000", "https://e-commerce-front-psi.vercel.app"],
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     credentials: true,
+//     allowedHeaders: ['Content-Type', 'Authorization']
+//   }));  
+  
 app.use(cors({
-    origin: "https://e-commerce-front-psi.vercel.app",
+    origin: ["http://localhost:3000", "https://e-commerce-front-psi.vercel.app"],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
-    allowedHeaders: ['Content-Type']
+    allowedHeaders: ['Content-Type','Authorization']
 }));
+
+
 app.use(express.json());
 app.use(morgan('dev'));
+
+// const __dirname = path.dirname("")
+// const buildpath = path.join(__dirname, "./frontend/build")
+// app.use(express.static(buildpath));
+// app.use(express.static(path.join(__dirname, './frontend/build')))
 
 //routes
 app.use("/api/v1/auth", authRoutes);
 app.use('/api/v1/category', categoryRoutes);
 app.use('/api/v1/product', productRoutes)
 
-//rest api
+// rest api
+// app.use('*', function(req,res){
+//     res.send(path.join(__dirname, './frontend/build/index.html'))
+// })
+
 app.get('/', (req, res)=>{
     res.send({
-        message:"welcome to mern app"
+        message:"welcome to mern stack e-commercial app"
     })
 })
 
